24cf1e18fc2ad0ea02a65db7e655c5359a40aa6c
martingarote <martindambrowskigarrote@gmail.com>
2023-10-16 21:38:10 -0300

    Salvar alterações feitas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CustomerSelectComponent } from '../customer-select/customer-select.component';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   public selectForm: FormGroup;
+  public filterComponent = ''
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -25,5 +27,12 @@ export class HomeComponent implements OnInit {
     const aaaa = this.selectForm.value;
     console.log(aaaa)
   }
+
+  public displayFilterComponent() {
+    const expectedComponentType = this.selectForm.value;
+    console.log("AAAAAA")
+  }
+
+  // precisa definir filtercomponent referente ao que foiselecionado para que ele atraves do ngif mostre o componente desejado
 
 }
